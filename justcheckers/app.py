c51123767c21f0ea753cb50edca7f369c35b3dfb
Dorian Pula <dorian.pula@amber-penguin-software.ca>
2014-07-30 08:33:43 -0400

    Move image and text assets into justcheckers folder. \n Delete old Android main menu setup. \n Recreate main menu in PySide.

--- justcheckers/app.py
+++ justcheckers/app.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from PySide.QtCore import QCoreApplication
-from PySide.QtGui import QWidget, QApplication, QLabel, QPushButton, QDesktopWidget, QVBoxLayout
+from PySide.QtGui import *
 
 
 class MenuScreen(QWidget):
@@ -13,17 +13,41 @@ class MenuScreen(QWidget):
         super(MenuScreen, self).__init__()
         self.setWindowTitle('justCheckers')
         self.setGeometry(300, 300, 800, 600)
+
+        self.setWindowIcon(QIcon('images/icon.png'))
+        self.add_backdrop()
+
         self.setup_components()
         self.center()
+
+    def add_backdrop(self):
+        tile = QPixmap('images/backdrop.jpg')
+        palette = QPalette()
+        palette.setBrush(QPalette.Background, tile)
+        self.setPalette(palette)
 
     def setup_components(self):
         """Setup the components that make up the widget."""
         print(self.get_system_info())
+
+        self.new_game = QPushButton('&New Game', self)
+        self.open_game = QPushButton('&Open Game', self)
+        self.save_game = QPushButton('&Save Game', self)
+        # TODO Render buttons greyed out.
+
+        self.about_game = QPushButton('About Game', self)
+        # TODO Add links to site and display license inside about game widget.
+        self.settings = QPushButton('Settings', self)
         self.exit_button = QPushButton('Exit', self)
         self.exit_button.clicked.connect(self.exit_app)
 
         widget_layout = QVBoxLayout(self)
         widget_layout.addStretch()
+        widget_layout.addWidget(self.new_game)
+        widget_layout.addWidget(self.open_game)
+        widget_layout.addWidget(self.save_game)
+        widget_layout.addWidget(self.about_game)
+        widget_layout.addWidget(self.settings)
         widget_layout.addWidget(self.exit_button)
         widget_layout.addStretch()
         self.setLayout(widget_layout)
